d0389603a1ddbc9ceaa62f782a99f0bc26281f68
Vladimir Vitkov <vvitkov@linux-bg.org>
2017-10-19 21:37:28 +0300

    Simple config regen with pw

---
+++ switch-configs/regen.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Make those configs usable
+#
+
+# Find directory in which this script is stored by traversing all symbolic links
+SOURCE="${0}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$(readlink "$SOURCE")"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+BASEDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+source ${BASEDIR}/secrets.conf
+
+for _config in $(ls -1 ${BASEDIR}/*.cfg) ; do
+	sed \
+		-e "s/@PW@/${ACCESS_PW}/g" \
+		-e "s/@EN_PW@/${ENABLE_PW}/g" \
+		-e "s/@SNMP_PW@/${SNMP_PW}/g" \
+		${_config} > ${_config%*\.cfg}_compiled.cfg
+done
